73c95d66f533ef97016555f9bd1cd5d0673d91a4
Manuel Espinosa <manuel.espinosa@live.com>
2025-05-02 14:11:34 -0600

    add DbSet to DbContext

--- src/AztecDateTranslator.Shared/AztecContext.cs
+++ src/AztecDateTranslator.Shared/AztecContext.cs
@@ -1,6 +1,22 @@
-﻿namespace AztecDateTranslator.Shared;
+﻿using AztecDateTranslator.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
 
-internal class AztecContext
+namespace AztecDateTranslator.Shared;
+
+internal class AztecContext : DbContext
 {
+    private readonly string _dbPath;
+    public AztecContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        _dbPath = Path.Join(path, "aztec.db");
+    }
+    public DbSet<DaySign> DaySigns { get; set; }
 
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlite($"Data Source={_dbPath}");
+        base.OnConfiguring(options);
+    }
 }
