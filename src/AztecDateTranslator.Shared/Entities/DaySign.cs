8c5ea2bc31c1441878e79a8dc57460491ac9a133
Manuel Espinosa <manuel.espinosa@live.com>
2025-05-02 14:06:04 -0600

    reordering members

--- src/AztecDateTranslator.Shared/Entities/DaySign.cs
+++ src/AztecDateTranslator.Shared/Entities/DaySign.cs
@@ -1,15 +1,26 @@
 namespace AztecDateTranslator.Shared.Entities;
 
+/// <summary>
+/// Represents one of the 20 aztec day signs.
+/// </summary>
 public class DaySign : BaseEntity
 {
-    public int Number { get; set; }
+    public Naguales Nagual { get; set; }
+
+    public byte DayNumber { get; set; }
 
     public string NahuatlName { get; set; }
         = string.Empty;
 
     public string SpanishName { get; set; }
+        = string.Empty;
 
     public string EnglishName { get; set; }
+        = string.Empty;
 
-    public Naguales Nagual { get; set; }
+    /// <summary>
+    /// Náhuatl name of Aztec deity associated to this day.
+    /// </summary>
+    public string AztecDeity { get; set; }
+        = string.Empty;
 }
