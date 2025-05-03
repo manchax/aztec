58185b5681f8f6a2a125d070ca4c5256176473d0
Manuel Espinosa <manuel.espinosa@live.com>
2025-05-03 09:06:40 -0600

    creating Tonalpohualli model

---
+++ src/AztecDateTranslator.Shared/Model/Tonalpohualli.cs
@@ -0,0 +1,42 @@
+﻿using AztecDateTranslator.Shared.Entities;
+
+namespace AztecDateTranslator.Shared.Model;
+/*
+
+Moon Calendar System
+
+ 
+This was a type of Aztec calendar dedicated to dates considered mystical and organized around a 260-day year,
+with 20 months of 13 days each.
+
+This calendar functioned as a record of auspicious dates for important events, such as the best days for sowing, 
+harvesting, or making expeditionary journeys.
+
+In the Tonalpohualli, each of the 260 days of the year have a name created from a system 
+that combines the names of the 20 days of the solar calendar with a numbering system between 1 and 13. 
+
+This system avoided the repetition of names.
+
+Thus, the first week of the civil calendar began on 
+1 Cipactli (1-alligator) and ended on 13 Acatl (13-reed). 
+
+The second week started at 1 Ocelotl (1- Jaguar) and the third at 1 Mázatl (1- deer).
+
+*/
+
+
+/// <summary>
+/// Moon based calendar.
+/// </summary>
+public class Tonalpohualli
+{
+    /// <summary>
+    /// 1-13
+    /// </summary>
+    public byte HeavenNumber { get; set; }
+
+    /// <summary>
+    /// 20 glyphs
+    /// </summary>
+    public DaySign? DaySign { get; set; }
+}
