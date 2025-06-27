a2dc24c3129180f03e665be5875c889fb3aaa20c
Manuel Espinosa <manuel.espinosa@live.com>
2025-06-27 17:36:34 -0600

    UI initial design

--- src/AztecDateTranslator/MainPage.xaml.cs
+++ src/AztecDateTranslator/MainPage.xaml.cs
@@ -13,12 +13,12 @@
         {
             count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            //if (count == 1)
+            //    CounterBtn.Text = $"Clicked {count} time";
+            //else
+            //    CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            //SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 
