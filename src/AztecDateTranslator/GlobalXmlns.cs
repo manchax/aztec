e0fc5f59186efc43cb1c9c6def95dd226b1345a3
Manuel Espinosa <manuel.espinosa@live.com>
2025-07-08 17:22:48 -0600

    Add local ViewModels to global namespace

--- src/AztecDateTranslator/GlobalXmlns.cs
+++ src/AztecDateTranslator/GlobalXmlns.cs
@@ -1,5 +1,9 @@
 ﻿using XmlnsPrefixAttribute = Microsoft.Maui.Controls.XmlnsPrefixAttribute;
 
+[assembly: XmlnsDefinition(
+    "http://schemas.microsoft.com/dotnet/maui/global",
+    "AztecDateTranslator.ViewModels")]
+
 [assembly: XmlnsPrefix(
     "clr-namespace:AztecDateTranslator.ViewModels;assembly=AztecDateTranslator",
     "vm")]
