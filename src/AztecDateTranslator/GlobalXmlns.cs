189a74709d7ea62193968235565b6182206697cd
Manuel Espinosa <manuel.espinosa@live.com>
2025-07-08 11:48:30 -0600

    implicit xaml namespaces

---
+++ src/AztecDateTranslator/GlobalXmlns.cs
@@ -0,0 +1,5 @@
+﻿using XmlnsPrefixAttribute = Microsoft.Maui.Controls.XmlnsPrefixAttribute;
+
+[assembly: XmlnsPrefix(
+    "clr-namespace:AztecDateTranslator.ViewModels;assembly=AztecDateTranslator",
+    "vm")]
